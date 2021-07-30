024bb1017af76ca51ab4ae8e212db618a272a4eb
Renan Fermino <renanfermino.jc@outlook.com>
2021-07-29 22:03:42 -0300

    Primeiro projeto com react-js
    Neste modulo esta sendo ensinado alguns conceitos basicos de estilizacao
    lifecycle dos componentes, e padronizacao de codigo.

---
+++ rocketseat/gostack11/trilha-antiga/fase3/primeiro-projeto/src/pages/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>
+                <FaGithub/>
+                Repositórios
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input
+                    type="text"
+                    placeholder="Adicionar Repositório"
+                />
+
+                <SubmitButton disabled>
+                    <FaPlus color="#FFF" size={14}/>
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
+}
