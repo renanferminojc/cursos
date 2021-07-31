cad263aeecfc5461ca5e53b2f6ca071abfd1f2cd
Renan Fermino <renanfermino.jc@outlook.com>
2021-07-31 14:59:34 -0300

    Adicionando repositorios por um form

--- rocketseat/gostack11/trilha-antiga/fase3/primeiro-projeto/src/pages/Main/index.js
+++ rocketseat/gostack11/trilha-antiga/fase3/primeiro-projeto/src/pages/Main/index.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithub/>
-                Repositórios
-            </h1>
+export default class Main extends React.Component {
+    state = {
+        newRepo: '',
+        repositories: [],
+        loading: false,
+    };
 
-            <Form onSubmit={() => {}}>
-                <input
-                    type="text"
-                    placeholder="Adicionar Repositório"
-                />
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
 
-                <SubmitButton disabled>
-                    <FaPlus color="#FFF" size={14}/>
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        this.setState({ loading: true });
+
+        const response = await api.get(`/repos/${this.state.newRepo}`);
+
+        console.log(response);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({
+            repositories: [...this.state.repositories, data],
+            newRepo: '',
+            loading: false,
+        });
+
+    };
+
+    render() {
+        const { newRepo, loading } = this.state;
+        return (
+            <Container>
+                <h1>
+                    <FaGithub/>
+                    Repositórios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar Repositório"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={14}/>
+                        ) : (
+                            <FaPlus color="#FFF" size={14}/>
+                        )}
+
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
--- rocketseat/gostack11/trilha-antiga/fase3/primeiro-projeto/src/pages/Main/styles.js
+++ rocketseat/gostack11/trilha-antiga/fase3/primeiro-projeto/src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -13,7 +13,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    
+
     svg {
       margin-right: 10px;
     }
@@ -34,9 +34,20 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
     type: 'submit',
-})`
+    disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -46,4 +57,18 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props =>
+    props.loading &&
+      css`  
+        svg {
+          animation: ${rotate} 2s linear infinite;
+        }
+      `
+    }
 `;
