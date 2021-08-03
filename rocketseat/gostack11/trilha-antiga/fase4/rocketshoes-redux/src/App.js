634362bf7293cbb689442ebccf3b41931203b238
Renan Fermino <renanfermino.jc@outlook.com>
2021-08-02 21:31:55 -0300

    Estilos globais da aplicacao

--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <BrowserRouter>
       {/*<Header />*/}
+      <GlobalStyle/>
       <Routes/>
     </BrowserRouter>
   );
