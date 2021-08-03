6dc144b453b0f4ff1232c905b12923e8bf9addc0
Renan Fermino <renanfermino.jc@outlook.com>
2021-08-02 21:55:12 -0300

    Criando o header da aplicacao

--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import Header from './components/Header';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
-      {/*<Header />*/}
+      <Header />
       <GlobalStyle/>
       <Routes/>
     </BrowserRouter>
