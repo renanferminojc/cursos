a19e2d076a5003b6d7f3c064c3cd4ef0a941a724
Renan Fermino <renanfermino.jc@outlook.com>
2021-08-05 23:09:31 -0300

    Removendo item do carrinho

--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/pages/Cart/index.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/pages/Cart/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -43,7 +43,7 @@ function Cart({ cart }) {
               <strong>R$258,80</strong>
             </td>
             <td>
-              <button type="button">
+              <button type="button" onClick={() => dispatch({ type: 'REMOVE_FROM_CART', product: { id: product.id } })}>
                 <MdDelete size={20} color="#7159c1"/>
               </button>
             </td>
@@ -66,7 +66,7 @@ function Cart({ cart }) {
 
 const mapStateToProps = state => ({
   cart: state.cart
-})
+});
 
 export default connect(mapStateToProps)(Cart);
 
--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/reducer.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/reducer.js
@@ -1,18 +1,23 @@
 export default function cart(state = [], action) {
+  const productIndex = state.findIndex(p => p.id === action.product.id);
   switch (action.type) {
     case 'ADD_TO_CART':
-      const productIndex = state.findIndex(p => p.id === action.product.id);
-
-      if(productIndex >= 0) {
-        state[productIndex].amount++
+      if (productIndex >= 0) {
+        state[productIndex].amount++;
         return [...state];
       } else {
         return [...state, {
           ...action.product,
           amount: 1
-        }]
+        }];
       }
-
+    case 'REMOVE_FROM_CART':
+      if (productIndex >= 0) {
+        state.splice(productIndex, 1);
+        return [...state];
+      } else {
+        return state;
+      }
     default:
       return state;
   }
