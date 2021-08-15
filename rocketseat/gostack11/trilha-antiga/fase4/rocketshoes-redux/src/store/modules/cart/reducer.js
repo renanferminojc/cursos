62659c540ea70af7f6c3d87f0b6baf3e81e40b20
Renan Fermino <renanfermino.jc@outlook.com>
2021-08-15 17:34:16 -0300

    Separando actions

--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/reducer.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/reducer.js
@@ -2,15 +2,9 @@ export default function cart(state = [], action) {
   const productIndex = state.findIndex(p => p.id === action.product.id);
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
-      if (productIndex >= 0) {
-        state[productIndex].amount++;
-        return [...state];
-      } else {
         return [...state, {
-          ...action.product,
-          amount: 1
+          ...action.product
         }];
-      }
     case '@cart/REMOVE':
       if (productIndex >= 0) {
         state.splice(productIndex, 1);
--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/sagas.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/store/modules/cart/sagas.js
@@ -1,14 +1,31 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../utils/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount} from './actions';
 
 function* addToCart({ product }) {
-  const response = yield call(api.get, `/products/${product.id}`)
+  const productExists = yield select(state => 
+    state.cart.find(p => p.id === product.id)
+    );
+    
+    if(productExists) {
+      const amount = productExists.amount + 1;
 
-  yield put(addToCartSuccess(response.data));
-}
+      yield put(updateAmount(product, amount));
+    } else {
+      const response = yield call(api.get, `/products/${product.id}`)
+     
+      const data = {
+        ...response.data,
+        amount: 1,
+        priceFormatted: formatPrice(response.data.price)
+      }
+
+      yield put(addToCartSuccess(data));
+    }
+  }
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
