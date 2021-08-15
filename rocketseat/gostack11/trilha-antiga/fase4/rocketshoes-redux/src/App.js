f4920bf234e98d4163ae3b45212a501ea623f1f9
Renan Fermino <renanfermino.jc@outlook.com>
2021-08-15 18:55:29 -0300

    Utilizando roteamento com o sagas

--- rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
+++ rocketseat/gostack11/trilha-antiga/fase4/rocketshoes-redux/src/App.js
@@ -5,18 +5,21 @@ import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 
 import Header from './components/Header';
-
 import GlobalStyle from './styles/global';
+
+// import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
+      {/* <Router history={history}> */}
       <BrowserRouter>
         <Header/>
         <GlobalStyle/>
         <Routes/>
         <ToastContainer autoClose={3000}/>
+      {/* </Router > */}
       </BrowserRouter>
     </Provider>
   );
